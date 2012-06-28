e1ec6f0d6ffd43f7497f2da4431a175e091f4a79
Elliot Wolk <elliot.wolk@gmail.com>
2012-06-28 16:32:31 -0400

    copied the config-files machine from n9, changed it for raspi, and added vim configs, bash configs, root symlinks, and some useful bins

---
+++ CONFIG_FILES/config-overwrite.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $DIR = '/opt/CONFIG_FILES';
+my @files = `ls -d $DIR/%*`;
+my $user = 'pi';
+my $group = 'pi';
+
+my @rsyncOpts = qw(
+  -a  --no-owner --no-group
+  --del
+  --out-format=%n
+);
+
+for my $file(@files){
+  chomp $file;
+  $file =~ s/^.*\///;
+  my $src = "$DIR/$file";
+  my $dest = $file;
+  $dest =~ s/%/\//g;
+  my $destDir = `dirname $dest`;
+  chomp $destDir;
+  system "mkdir -p $destDir";
+  print "\n%%% $dest\n";
+  if(-d $src){
+    system 'rsync', @rsyncOpts, "$src/", "$dest";
+  }else{
+    system 'rsync', @rsyncOpts, "$src", "$dest";
+  }
+  if($destDir =~ /^\/home\/pi/){
+    system "chown -R $user.$group $dest";
+    system "chown $user.$group $destDir";
+  }else{
+    system "chown -R root.root $dest";
+    system "chown root.root $destDir";
+  }
+}
+
+for my $file(`cat $DIR/config-files-to-remove`){
+  chomp $file;
+  if(-e $file){
+    if(-d $file){
+      $file =~ s/\/$//;
+      $file .= '/';
+      print "\nremoving these files in $file:\n";
+      system "find $file";
+    }else{
+      print "\nremoving $file\n";
+    }
+    system "rm -r $file";
+  }
+}
