2e28c0618b3f12d1ab9225c447b91c7846bede34
Elliot Wolk <elliot.wolk@gmail.com>
2012-07-04 16:27:29 -0400

    added install packages list

---
+++ packages.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my %pkgGroups = (
+  '1' => [qw(
+    vim-gtk rsync wget git openvpn
+  )],
+);
+
+sub installPackages();
+
+sub main(@){
+  system "raspi", "-s", "apt-get update";
+  system "raspi", "-s", "apt-get upgrade";
+  installPackages();
+}
+
+sub installPackages(){
+  print "\n\n";
+  for my $pkgGroup(sort keys %pkgGroups){
+    my @packages = @{$pkgGroups{$pkgGroup}}; 
+    print "Installing group[$pkgGroup]:\n----\n@packages\n----\n";
+    system "raspi", "-s", ''
+      . "yes |"
+      . " apt-get install"
+      . " -y --allow-unauthenticated"
+      . " @packages";
+  }
+}
+
+&main(@ARGV);
