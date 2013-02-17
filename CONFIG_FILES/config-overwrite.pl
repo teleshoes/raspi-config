3e047fdae7b4d3f51da5a13c4bf4a2b7c45d06bb
Elliot Wolk <elliot.wolk@gmail.com>
2013-02-16 23:30:30 -0500

    updated config-overwrite with more modern methods from n9

--- CONFIG_FILES/config-overwrite.pl
+++ CONFIG_FILES/config-overwrite.pl
@@ -2,6 +2,8 @@
 use strict;
 use warnings;
 
+my $hostName = "raspbmc";
+
 my $DIR = '/opt/CONFIG_FILES';
 my $user = 'pi';
 my $group = 'pi';
@@ -13,11 +15,22 @@ my @rsyncOpts = qw(
   --out-format=%n
 );
 
+my %symlinksToReplace = (
+);
+my %changedTriggers = (
+);
+
 sub overwriteFile($$);
 sub removeFile($);
+sub md5sum($);
 
 sub main(@){
-  die "Usage: $0\n" if @_ > 0;
+  my $type = shift;
+  $type = 'all' if not defined $type;
+  my $okTypes = join "|", qw(boing bin remove all);
+  die "Usage: $0 [$okTypes]\n" if @_ > 0 or $type !~ /^($okTypes)$/;
+  die "hostname must be $hostName" if `hostname` ne "$hostName\n";
+
   my @boingFiles = `cd $DIR; ls -d %*`;
   chomp foreach @boingFiles;
   my @binFiles = `cd $DIR/bin; ls -d *`;
@@ -25,22 +38,47 @@ sub main(@){
   my @filesToRemove = `cat $DIR/config-files-to-remove`;
   chomp foreach @filesToRemove;
 
-  print "\n ---handling boing files...\n";
-  for my $file(@boingFiles){
-    my $dest = $file;
-    $dest =~ s/%/\//g;
-    overwriteFile "$DIR/$file", $dest;
+  my %triggers;
+
+  if($type =~ /^(boing|all)$/){
+    print "\n ---handling boing files...\n";
+    for my $file(@boingFiles){
+      my $dest = $file;
+      $dest =~ s/%/\//g;
+      my ($old, $new);
+      if(defined $changedTriggers{$dest}){
+        $old = md5sum $dest;
+      }
+      overwriteFile "$DIR/$file", $dest;
+      if(defined $changedTriggers{$dest}){
+        $new = md5sum $dest;
+        if($old ne $new){
+          print "   ADDED TRIGGER: $changedTriggers{$dest}\n";
+          $triggers{$changedTriggers{$dest}} = 1;
+        }
+      }
+    }
   }
 
-  print "\n ---handling bin files...\n";
-  for my $file(@binFiles){
-    overwriteFile "$DIR/bin/$file", "$binTarget/$file";
+  if($type =~ /^(bin|all)$/){
+    print "\n ---handling bin files...\n";
+    for my $file(@binFiles){
+      overwriteFile "$DIR/bin/$file", "$binTarget/$file";
+    }
   }
 
-  print "\n ---removing files to remove...\n";
-  for my $file(@filesToRemove){
-    chomp $file;
-    removeFile $file;
+  if($type =~ /^(remove|all)$/){
+    print "\n ---removing files to remove...\n";
+    for my $file(@filesToRemove){
+      chomp $file;
+      removeFile $file;
+    }
+  }
+
+  print "\n ---running triggers...\n";
+  for my $trigger(keys %triggers){
+    print "  $trigger: \n";
+    system $trigger;
   }
 }
 
@@ -55,6 +93,12 @@ sub overwriteFile($$){
   }else{
     system 'rsync', @rsyncOpts, "$src", "$dest";
   }
+
+  if(defined $symlinksToReplace{$dest} and -l $dest){
+    my $realDest = readlink $dest;
+    system "cp", $realDest, $dest;
+  }
+
   if($destDir =~ /^\/home\/$user/){
     system "chown -R $user.$group $dest";
     system "chown $user.$group $destDir";
@@ -79,4 +123,15 @@ sub removeFile($){
   }
 }
 
+sub md5sum($){
+  my $file = shift;
+  my $out;
+  if(-d $file){
+    $out = `md5sum "$file"/* 2>/dev/null | sort`;
+  }else{
+    $out = `md5sum $file 2>/dev/null`;
+    chomp $out;
+  }
+  return $out;
+}
 &main(@ARGV);
