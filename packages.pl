2bd550c47e24d24b7e0cd6e3d923e4ff9f2661e0
Elliot Wolk <elliot.wolk@gmail.com>
2013-03-18 19:55:27 -0400

    dont remove network manager

--- packages.pl
+++ packages.pl
@@ -2,10 +2,6 @@
 use strict;
 use warnings;
 
-my @remove = qw(
-  network-manager
-);
-
 my %pkgGroups = (
   '1' => [qw(
     vim-gtk rsync wget git openvpn
@@ -27,8 +23,6 @@ my %pkgGroups = (
 sub installPackages();
 
 sub main(@){
-  system "raspi", "-b", "apt-get purge @remove";
-  system "raspi", "-b", "apt-get autoremove";
   system "raspi", "-b", "apt-get update";
   system "raspi", "-b", "apt-get upgrade -y";
   installPackages();
