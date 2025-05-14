b8668feeeb80a9e5d8aece13a4b715d2aaecbc3d
Elliot Wolk <elliot.wolk@gmail.com>
2025-05-14 13:50:13 -0400

    flash: add flash.pl wrapper around ddusb + bash read

---
+++ flashing/flash.pl
@@ -0,0 +1,86 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use File::Basename qw(basename);
+
+sub prompt($$);
+sub run(@);
+
+my $IMG = "image_to_flash.img";
+my $IMG_XZ = "$IMG.xz";
+
+my $EXEC = basename $0;
+
+my $USAGE = "Usage:
+  $EXEC -h | --help
+    show this message
+
+  $EXEC
+    flash $IMG using ddusb
+      NOTE: runs lsblk and prompts before writing to block devices
+";
+
+sub main(@){
+  while(@_ > 0){
+    my $arg = shift @_;
+    if($arg =~ /^(-h|--help)$/){
+      print $USAGE;
+      exit 0;
+    }else{
+      die "$USAGE\nERROR: unknown arg $arg\n";
+    }
+  }
+
+  run "lsblk";
+  print "\n\n";
+
+  my $dev = prompt("ENTER BLOCK DEVICE OF SDCARD: ", "/dev/sdb");
+  die "ERROR: \"$dev\" is not a block device\n" if not -b $dev;
+
+  if(not -f $IMG and -f $IMG_XZ){
+    print "echo decompressing $IMG_XZ\n";
+    run "xz -d -k $IMG_XZ";
+  }
+  die "ERROR: missing $IMG\n" if not -f $IMG;
+
+  if(promptYesNo("\nddusb $IMG $dev\nRUN THE ABOVE CMD?")){
+    run "ddusb", $IMG, $dev;
+  }
+}
+
+sub promptYesNo($){
+  my ($msg) = @_;
+  my $val = prompt("$msg [y/N] ", undef);
+  if($val =~ /^(y|yes)$/i){
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
+sub prompt($$){
+  my ($msg, $default) = @_;
+  print $msg;
+
+  $default = "" if not defined $default;
+  my $val = `bash -c 'read -e -i "$default" PROMPT; echo "\$PROMPT"'`;
+  chomp $val;
+  $val =~ s/^\s*//;
+  $val =~ s/\s*$//;
+
+  print "\n" if $val eq "";
+
+  if($val =~ /[ \t\r\n]/){
+    die "ERROR: prompt for single value resulted in '$val'\n";
+  }
+
+  return $val;
+}
+
+sub run(@){
+  print "@_\n";
+  system @_;
+  die "ERROR: command '@_' failed\n" if $? != 0;
+}
+
+&main(@ARGV);
